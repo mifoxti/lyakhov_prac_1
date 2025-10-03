e63fd8e4207b3710c69195389f5ea9af0171ad0e
mifoxti <52703409+mifoxti@users.noreply.github.com>
2025-10-03 18:12:48 +0300

    Изменение в main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,21 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MiMusicApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MiMusicApp extends StatelessWidget {
+  const MiMusicApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Ляхов Тимофей Алексеевич',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.deepPurple,
-                  ),
-                ),
-              ),
-              const SizedBox(width: 20),
-              Container(
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  color: Colors.deepPurple[100],
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: ElevatedButton(
-                  onPressed: null,
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.deepPurple),
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                    ),
-                  ),
-                  child: Text(
-                    'Пример кнопки',
-                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+      title: 'MiMusic',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
       ),
+      home: const HomeScreen(),
     );
   }
 }
