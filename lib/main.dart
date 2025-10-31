5119dd44213f98a8d2138044d34ee2df7f745202
mifoxti <52703409+mifoxti@users.noreply.github.com>
2025-10-31 21:44:51 +0300

    Реализация горизонтальной навигации в файле main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'app_router.dart';
+import 'features/library/screens/library_screen.dart';
+import 'features/player/state/player_container.dart';
+import 'features/search/screens/search_screen.dart';
+import 'features/profile/screens/profile_screen.dart';
 
 void main() {
   runApp(const MiMusicApp());
@@ -11,13 +13,91 @@ class MiMusicApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    return MaterialApp(
       title: 'MiMusic',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
+        useMaterial3: true,
       ),
-      routerConfig: appRouter,
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const LibraryScreen(),
+    PlayerContainer.withScreen(),
+    const SearchScreen(),
+    const ProfileScreen(),
+  ];
+
+  final List<String> _appBarTitles = [
+    'Моя библиотека',
+    'Сейчас играет',
+    'Поиск музыки',
+    'Мой профиль'
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[50],
+      appBar: AppBar(
+        title: Text(
+          _appBarTitles[_currentIndex],
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+      ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.deepPurple,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.deepPurple[100],
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_music),
+            label: 'Библиотека',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.play_circle_fill),
+            label: 'Плеер',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Поиск',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Профиль',
+          ),
+        ],
+      ),
     );
   }
 }
